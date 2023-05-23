ac16e43ff676a9b8f00e5a1fa777e82da110504c
Abduvokhidjon <aabduvohid33@gmail.com>
2023-05-23 18:10:48 -0500

    first commit

---
+++ target.js
@@ -0,0 +1,12 @@
+num1 = 4
+num2 = 8
+target = 12
+result = num1 + num2
+result == target
+console.log ("If numbers meet the target: ",result == target)
+
+num1 = 4
+num2 = 8
+target2 = 4
+result2 = num1 - num2
+console.log("if subtraction meet the target: ",result2 == target2)
